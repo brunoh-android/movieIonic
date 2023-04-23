fb803bc71bbbe85145e2877c4255bb5e1cd79baa
Bruno <bruni.henriqe@gmail.com>
2023-04-23 20:15:06 -0300

    movie app ionic

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'a1cecfbc98de744b54f4e97192be274f',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
 
 /*
